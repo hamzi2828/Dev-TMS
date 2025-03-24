d16264cca30f90dd44d3517f58bbb897df2921b4
hamza jahangir <hamzahashmi640@gmail.com>
2025-03-24 14:49:01 +0500

    c

--- resources/views/_partials/_sidebar.blade.php
+++ resources/views/_partials/_sidebar.blade.php
@@ -17,7 +17,6 @@
 
     <ul class="menu-inner py-1">
         @include('_partials.menus.dashboard')
-        @include('_partials.menus.data-banks')
         @include('_partials.menus.accounts')
         @include('_partials.menus.accounts-settings')
         @include('_partials.menus.accounts-reporting')
