1221303a8aee56b6275d25c26a5ec75255f83691
hamza jahangir <hamzahashmi640@gmail.com>
2025-05-31 11:30:37 +0500

    s

--- resources/views/my-bookings/create.blade.php
+++ resources/views/my-bookings/create.blade.php
@@ -234,7 +234,7 @@
             function formatCurrency(amount) {
                 return new Intl.NumberFormat('en-PK', {
                     style: 'currency',
-                    currency: ' ',
+                    currency: 'PKR',
                     minimumFractionDigits: 0,
                     maximumFractionDigits: 0
                 }).format(amount).replace(' ', '  ');
