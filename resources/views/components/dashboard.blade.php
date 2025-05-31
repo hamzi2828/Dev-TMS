f87c82f85041ed14948a0c5e1b6ae34b62da997d
hamza jahangir <hamzahashmi640@gmail.com>
2025-05-31 14:48:34 +0500

    c

--- resources/views/components/dashboard.blade.php
+++ resources/views/components/dashboard.blade.php
@@ -26,8 +26,9 @@
     <link rel="stylesheet" href="{{ asset('/assets/vendor/fonts/flag-icons.css') }}" />
 
     <!-- Core CSS -->
-    <link rel="stylesheet" href="{{ asset('/assets/vendor/css/rtl/core.css') }}" />
-    <link rel="stylesheet" href="{{ asset('/assets/vendor/css/rtl/theme-default.css') }}" />
+    <link rel="stylesheet" href="{{ asset('/assets/vendor/css/rtl/core.css') }}" class="template-customizer-core-css" />
+    <link rel="stylesheet" href="{{ asset('/assets/vendor/css/rtl/theme-default.css') }}"
+          class="template-customizer-theme-css" />
     <link rel="stylesheet" href="{{ asset('/assets/css/demo.css') }}" />
 
     <!-- Vendors CSS -->
@@ -58,6 +59,9 @@
 
     <!-- Helpers -->
     <script src="{{ asset('/assets/vendor/js/helpers.js') }}"></script>
+    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js') }} in the <head> section -->
+    <!--? Template customizer: To hide customizer set displayCustomizer value false in config.js') }}.  -->
+    <script src="{{ asset('/assets/vendor/js/template-customizer.js') }}"></script>
     <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
     <script src="{{ asset('/assets/js/config.js') }}"></script>
 </head>
@@ -129,16 +133,6 @@
 <script src="{{ asset ('/assets/vendor/libs/@form-validation/umd/plugin-auto-focus/index.min.js') }}"></script>
 <script src="{{ asset ('/assets/vendor/libs/moment/moment.js') }}"></script>
 <script src="{{ asset('/assets/js/custom.js?ver='.rand ()) }}"></script>
-
-            <!-- Flatpickr JS -->
-            <script>
-                document.addEventListener('DOMContentLoaded', function() {
-                    flatpickr('.flatpickr-basic', {
-                        dateFormat: 'Y-m-d',
-                        allowInput: true
-                    });
-                });
-            </script>
 @stack('scripts')
 </body>
 </html>
